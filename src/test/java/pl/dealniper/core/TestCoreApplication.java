960fe672c8d810fd6c42a39a9121058a90b1366c
OskarCh29 <oskarcharytoniuk29@gmail.com>
2025-05-27 23:26:18 +0200

    first commit

---
+++ src/test/java/pl/dealniper/core/TestCoreApplication.java
@@ -0,0 +1,11 @@
+package pl.dealniper.core;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(CoreApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
