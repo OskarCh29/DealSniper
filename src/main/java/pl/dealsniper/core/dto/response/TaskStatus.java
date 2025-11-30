c3245ef116973fee6c361deefbb2689b3d0dcc1b
OskarCh29 <oskarcharytoniuk29@gmail.com>
2025-11-30 16:31:02 +0100

    chore: Add constructor parameter

--- src/main/java/pl/dealsniper/core/dto/response/TaskStatus.java
+++ src/main/java/pl/dealsniper/core/dto/response/TaskStatus.java
@@ -1,16 +1,18 @@
 /* (C) 2025 */
 package pl.dealsniper.core.dto.response;
 
+import lombok.Getter;
+
+@Getter
 public enum TaskStatus {
     STARTED("TASK STARTED SUCCESSFULLY"),
-    STOPPED("TASK STOPPED SUCESSFULLY"),
+    STOPPED("TASK STOPPED SUCCESSFULLY"),
     RESUMED("TASK RESUMING IN PROGRESS ");
 
     private String message;
 
-    TaskStatus(String message) {}
-
-    public String getMessage() {
-        return message;
+    TaskStatus(String message) {
+        this.message = message;
     }
+
 }
